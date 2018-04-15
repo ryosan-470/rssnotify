4c34d46adf44d0356a41bf918c0a83b608198f33
Ryosuke Sato <rskjtwp@gmail.com>
2018-04-15 18:24:21 +0900

    Add IsUpdated

--- util.go
+++ util.go
@@ -1,12 +1,19 @@
 package main
 
-import "time"
-
-var now = time.Now()
+import (
+	"time"
+)
 
 // ToTime converts argument to time.Time
 func ToTime(t string) time.Time {
 	// "Fri, 13 Apr 2018 09:06:00 GMT"
 	time, _ := time.Parse(time.RFC1123, t)
 	return time
+}
+
+// IsUpdated は updated が 現在時刻から interval (min) 以内に更新されたかどうかを判定する
+func IsUpdated(interval int, updated, now time.Time) bool {
+	t := time.Duration(interval) * time.Minute
+	newUpdated := updated.Add(t)
+	return newUpdated.After(now)
 }
--- util_test.go
+++ util_test.go
@@ -21,3 +21,38 @@ func TestToTime(t *testing.T) {
 		}
 	}
 }
+
+func TestIsUpdated(t *testing.T) {
+	loc, _ := time.LoadLocation("UTC")
+	testCases := []struct {
+		updated, now time.Time
+		interval     int
+		expected     bool
+	}{
+		{
+			interval: 5,
+			updated:  time.Date(2000, time.January, 1, 0, 0, 0, 0, loc),
+			now:      time.Date(2000, time.January, 1, 0, 4, 59, 99, loc),
+			expected: true,
+		},
+		{
+			interval: 5,
+			updated:  time.Date(2000, time.January, 1, 0, 0, 0, 0, loc),
+			now:      time.Date(2000, time.January, 1, 0, 5, 0, 0, loc),
+			expected: false,
+		},
+		{
+			interval: 5,
+			updated:  time.Date(2000, time.January, 1, 0, 0, 0, 0, loc),
+			now:      time.Date(2000, time.January, 1, 0, 5, 0, 1, loc),
+			expected: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		actual := IsUpdated(testCase.interval, testCase.updated, testCase.now)
+		if actual != testCase.expected {
+			t.Errorf("\ngot %v\nwant %v", actual, testCase.expected)
+		}
+	}
+}
